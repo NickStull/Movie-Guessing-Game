e5d643f3ab1702e24de4aff374859bd6972e796c
Bardini <derekbardini@gmail.com>
2021-01-16 21:38:57 -0600

    added cardContainer and guessButton ids, and changed color of my-card-divider

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var movieList = [
 ]
 var movieTitle = movieList[Math.floor(Math.random() * movieList.length)];
 var container = $(".container");
-var omdbAPIKey = ""
+var omdbAPIKey = "trilogy"
 var giphyAPIKey = ""
 var score = 100
 var movieInfo = {}
