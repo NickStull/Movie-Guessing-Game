a603a9c05853732cb42ced0959d3a47900c8e4a4
Mandelbrot <timmartin13@gmail.com>
2021-01-17 18:36:52 -0600

    GOt the modal to pop

--- assets/js/walkoffame.js
+++ assets/js/walkoffame.js
@@ -1,10 +1,16 @@
-//<div class="card-container">
-//     <div class="star-card">
-//         <img src="./assets/images/walkofFameStar.jpg" alt="Walk of fame star">
-//     </div>
-//     <div class="star-card-text">Tim Martin: 20</div>
-// </div>
-let highscores = ["Tim Martin: 30", "Jimi Simon: 30", "Derek Bardini: 30", "Nick Stull: 30"];
+$(document).foundation();
+
+let storedScore = 17;
+let userScore = 18;
+
+if (userScore > storedScore) {
+    $("#exampleModal1").foundation("open");
+}
+// let h1Tag = $("<h1>");
+// h1Tag.text("Awesome. I have it.");
+// $("#exampleModal1").append(h1Tag);
+
+let highscores = ["Tim Martin: 30", "Jimi Simon: 30", "Derek Bardini: 30", "Nick Stull: 30", "Aubrey Plaza: 24", "Morgan Freeman: 22", "Olivia Munn: 21", "Jim Carrey: 20", "Ryan Reynolds: 20", "Will Smith: 18"];
 let myGrid = $("#my-grid");
 
 for (let i = 0; i < highscores.length; i++) {
